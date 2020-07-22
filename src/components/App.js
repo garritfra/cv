3efbfe00b87d1fa7d7cf8932fda3eee920cf236f
garritfra <garritfranke@gmail.com>
2020-07-22 17:21:18 +0200

    feat: position event

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Event from "./Event";
 
 const Container = styled.div`
-  text-align: center;
+  align-items: center;
 `;
 
 export default function App() {
-  return (
-    <Container>
-      <Event title={"foo"} description="Lorem Ipsum" />
-      <Event title={"bar"} description="Lorem Ipsum" />
-      <Event title={"baz"} description="Lorem Ipsum" />
-    </Container>
-  );
+  const events = [
+    { year: 2020, title: "foo", description: "Lorem Ipsum" },
+    { year: 2019, title: "bar", description: "Lorem Ipsum" },
+  ];
+
+  const eventsComponents = events.map((event) => (
+    <SpacedEvent
+      title={event.title}
+      description={event.description}
+      year={event.year}
+    />
+  ));
+
+  return <Container>{eventsComponents}</Container>;
 }
+
+const SpacedEvent = styled(Event)`
+  margin: 2em;
+`;
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Event({ title, description }) {
+export default function Event({ title, description, year }) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
+    <Container>
+      <YearContainer>
+        <Title>{year}</Title>
+      </YearContainer>
+      <ContentContainer>
+        <Title>{title}</Title>
+        <p>{description}</p>
+      </ContentContainer>
+    </Container>
   );
 }
+
+const YearContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-right: 1em;
+`;
+
+const ContentContainer = styled.div``;
+
+const Container = styled.div`
+  display: flex;
+  margin: 1em;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+`;
