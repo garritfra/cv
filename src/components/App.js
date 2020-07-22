2912e34e4591f2dddffef0df241b21ed77f1dc9c
Garrit Franke <32395585+garritfra@users.noreply.github.com>
2020-07-22 16:47:20 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+export default function App() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  );
+}
